b25d7db81dea6d55a67ff00a44ada28d054c02eb
shodydosh <shodydosh@gmail.com>
2023-07-19 00:55:28 +0700

    feat: reponsive navbar for Header

--- FE/components/Core/Header.tsx
+++ FE/components/Core/Header.tsx
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Header: React.FC = () => {
-  const [scroll, setScroll] = useState(false);
+  const [scroll, setScroll] = useState<Boolean>(false);
+  const [isNavOpen, setNavOpen] = useState<Boolean>(false);
+
+  // Function to open the navigation menu
+  const openNav = () => {
+    setNavOpen(true);
+  };
+
+  // Function to close the navigation menu
+  const closeNav = () => {
+    setNavOpen(false);
+  };
+
+  // Function to toggle the navigation menu
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,96 +37,103 @@ const Header: React.FC = () => {
 
   return (
     <header
-      className={`pl-15 sticky top-0 z-20 flex px-4 py-4 text-white shadow-xl duration-500 md:px-10 xl:px-40 ${
+      className={`duration-500 flex flex-wrap lg:flex-nowrap items-center justify-between pl-15 sticky top-0 z-20 px-4 py-4 text-white shadow-xl duration-500 md:px-10 xl:px-40 ${
         scroll ? "bg-white" : "bg-slate-950"
       }`}
     >
-      <div className="w-full">
-        <Link
-          href="./"
-          className={`text-bold text-2xl ${
-            scroll ? "text-slate-950" : "text-white"
-          }`}
-        >
-          TOOL
-        </Link>
-      </div>
-      <div className="hidden w-full grid-cols-4 content-center duration-500 lg:grid">
-        <Link
-          href="./about"
-          className={`text-md px-2 text-center duration-500 hover:opacity-90 ${
-            scroll ? "text-slate-950" : "text-white"
-          }`}
-        >
-          About
-        </Link>
-        <Link
-          href="./pricing"
-          className={`text-md px-2 text-center duration-500 hover:opacity-90 ${
-            scroll ? "text-slate-950" : "text-white"
-          }`}
-        >
-          Pricing
-        </Link>
-        <Link
-          href="./tool"
-          className={`text-md px-2 text-center duration-500 hover:opacity-90 ${
-            scroll ? "text-slate-950" : "text-white"
-          }`}
-        >
-          Tool
-        </Link>
-        <Link
-          href="./contact"
-          className={`text-md px-2 text-center duration-500 hover:opacity-90 ${
-            scroll ? "text-slate-950" : "text-white"
-          }`}
-        >
-          Contact
-        </Link>
-      </div>
-      <div className="hidden w-full flex-col-reverse items-end lg:flex">
-        <div>
-          <button
-            className={`mr-2 border px-6 py-1 duration-500 hover:text-slate-950 ${
-              scroll
-                ? "border-slate-950 text-slate-950 hover:bg-slate-950 hover:text-white"
-                : "border-white hover:bg-white hover:text-slate-950"
+        <div className="flex items-center duration-500">
+          <Link
+            href="./"
+            className={`text-bold text-2xl ${
+              scroll ? "text-slate-950" : "text-white"
             }`}
           >
-            <Link href="./login">Login</Link>
-          </button>
-          <button
-            className={`border px-6 py-1 duration-500 ${
-              scroll
-                ? "border-slate-950 bg-slate-950 text-white hover:bg-white hover:text-slate-950"
-                : "bg-white text-slate-950 hover:bg-slate-950 hover:text-white"
-            }`}
-          >
-            <Link href="./signup">Sign up</Link>
-          </button>
+            TOOL
+          </Link>
         </div>
-      </div>
-      <button
-        className={`mx-2 block duration-500 lg:hidden ${
-          scroll ? "text-slate-950" : "text-white"
-        }`}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6"
+        <button
+        onClick={toggleNav}
+          className={`duration-500 inline-flex items-center justify-center w-10 h-10 p-2 duration-500 lg:hidden ${
+            scroll ? "text-slate-950" : "text-white"
+          }`}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="w-full duration-500 xk:ml-60 lg:ml-40 md:justify-center md:content-center lg:grid">
+          <ul className={`duration-500 flex flex-col p-4 mt-4 font-medium lg:mt-0 lg:flex-row lg:space-x-8 lg:border-0 lg:p-0 lg:dark:bg-gray-900 ${isNavOpen ? "hidden" : "flex" }`}>
+            <li>
+              <Link
+                href="./contact"
+                className={`block py-2 pl-3 pr-4 rounded 
+                ${scroll ? "text-slate-950" : "text-white"}
+                `}>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="./contact"
+                className={`block py-2 pl-3 pr-4 rounded 
+                ${scroll ? "text-slate-950" : "text-white"}
+                `}>
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="./contact"
+                className={`block py-2 pl-3 pr-4 rounded 
+                ${scroll ? "text-slate-950" : "text-white"}
+                `}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="./contact"
+                className={`block py-2 pl-3 pr-4 rounded
+                ${scroll ? "text-slate-950" : "text-white"}
+                `}>
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-col-reverse items-end hidden duration-500 w-96 lg:flex">
+          <div>
+            <button
+              className={`mr-2 border px-6 py-1 duration-500 hover:text-slate-950 ${
+                scroll
+                  ? "border-slate-950 text-slate-950 hover:bg-slate-950 hover:text-white"
+                  : "border-white hover:bg-white hover:text-slate-950"
+              }`}
+            >
+              <Link href="./login">Login</Link>
+            </button>
+            <button
+              className={`border px-6 py-1 duration-500 ${
+                scroll
+                  ? "border-slate-950 bg-slate-950 text-white hover:bg-white hover:text-slate-950"
+                  : "bg-white text-slate-950 hover:bg-slate-950 hover:text-white"
+              }`}
+            >
+              <Link href="./signup">Sign up</Link>
+            </button>
+          </div>
+        </div>
     </header>
   );
 };
