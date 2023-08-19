7283457c9f35a6ef95eacad23537175efb3bdc45
Phu <87368494+ppppHHHuuuu@users.noreply.github.com>
2023-08-19 21:15:20 +0700

    Delete PittyTest directory

--- PittyTest/data_path.py
+++
@@ -1,6 +0,0 @@
-import os
-import pathlib
-data = pathlib.Path(__file__).parent.resolve()
-print (data)
-data = pathlib.Path(__file__).pare
-print(data)
--- PittyTest/slitherFilter.py
+++
@@ -1,6 +0,0 @@
-class SlitherFilter:
-    #if take the smallest element the best, use this
-    @classmethod
-    def choose_smallest_element(cls, elements: list[str]):
-        return elements[0]
-            
